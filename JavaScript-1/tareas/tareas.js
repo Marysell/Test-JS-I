367641598324b2a91af560e26b7be8c59c73653b
Marysell <maricelgomez82@gmail.com>
2022-07-07 15:51:44 -0300

    tarea1

--- JavaScript-1/tareas/tareas.js
+++ JavaScript-1/tareas/tareas.js
@@ -74,11 +74,7 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
 
-  if(str1.lenght  == str2.lenght ){
-    return true;
-  }else{
-    return false;
-  }
+  return str1.length  == str2.length;
 }
 
 function menosQueNoventa(num) {
